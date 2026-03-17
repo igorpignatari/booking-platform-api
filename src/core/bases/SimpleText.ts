74b3b8fd290759097ecb1f373287be832c95556a
filhotera <igor.pignatari@hotmail.com>
2026-03-17 21:08:10 +0000

    fix(core/bases): SimpleText abstract class

--- src/core/bases/SimpleText.ts
+++ src/core/bases/SimpleText.ts
@@ -5,7 +5,7 @@ export abstract class SimpleText extends ValueObject<string> {
     super(simpleText);
   }
 
-  protected validateSimpleText(value: string, min = 3, max = 80): boolean {
+  protected static validateSimpleText(value: string, min = 3, max = 80): boolean {
     if (value === null || value === undefined) return false;
     if (value.length < min && value.length > max) return false;
     return true;
