46387b1b525431a23cd2f5e2bf0cda3d187dfe84
filhotera <igor.pignatari@hotmail.com>
2026-03-23 19:33:10 +0000

    refactor(contexts/users/infra): add findByEmailForAuth function

--- src/contexts/users/infra/database/repository/UserRepositoryInMemory.ts
+++ src/contexts/users/infra/database/repository/UserRepositoryInMemory.ts
@@ -1,17 +1,40 @@
-import type { CreateUserRequest } from "@contexts/users/application/DTOs/createUserDTO";
+import type { AuthUserRepository } from "@contexts/auth/application/ports/output/AuthUserRepository";
+import type { AuthUser } from "@contexts/auth/domain/entities/AuthUser";
+import { AuthUserMapper } from "@contexts/auth/infra/database/mapper/AuthUserMapper";
 import type { UserRepository } from "@contexts/users/application/ports/output/UserRespository";
 import type { User } from "@contexts/users/domain/entity/User";
 import { Result } from "@core/result/Result";
+import type { UserRow } from "../dao/type/UserRow";
 
-export class UserRepositoryInMemory implements UserRepository {
-  private users: any[] = [];
+export class UserRepositoryInMemory implements UserRepository, AuthUserRepository {
+  private users: User[] = [];
 
-  async create(user: CreateUserRequest): Promise<Result<null>> {
+  async create(user: User): Promise<Result<null>> {
     this.users.push(user);
     return Promise.resolve(Result.ok(null));
   }
-  findByEmail(email: string): Promise<Result<User | null>> {
-    const user = this.users.find((user) => user.email === email);
-    return Promise.resolve(Result.ok(user || null));
+  async findByEmail(email: string): Promise<Result<User | null>> {
+    const user = this.users.find((user) => user.email.getValue() === email);
+    if (!user) {
+      return Promise.resolve(Result.ok(null));
+    }
+    return Promise.resolve(Result.ok(user));
+  }
+  findByEmailForAuth(email: string): Promise<Result<AuthUser | null>> {
+    const user = this.users.find((user) => user.email.getValue() === email);
+    if (!user) {
+      return Promise.resolve(Result.ok(null));
+    }
+    const authUserRow: UserRow = {
+      id: user.id,
+      name: user.name,
+      email: user.email.getValue(),
+      password: user.password.getValue(),
+      phone: user.phone,
+      created_at: user.createdAt.toISOString(),
+      updated_at: user.updatedAt.toISOString(),
+      role: user.role,
+    };
+    return Promise.resolve(Result.ok(AuthUserMapper.toDomain(authUserRow)));
   }
 }
