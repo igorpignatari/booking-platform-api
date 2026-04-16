8d48bba59ed2a32a8b43ab1589a8dffd0c93ea2b
filhotera <igor.pignatari@hotmail.com>
2026-04-16 02:15:53 +0100

    refactor(core/valueObjects): improve email, password value objects tests

---
+++ src/core/valueObjects/__tests__/Email.test.ts
@@ -0,0 +1,20 @@
+import { Email } from "@core/valueObjects/Email";
+
+describe("Email value object", () => {
+  describe("sucess", () => {
+    it("should create a valid email", () => {
+      expect(Email.create("joe_doe@example.com").isOk).toBe(true);
+      expect(Email.create("joe_doe@example.com").value.getValue()).toBe("joe_doe@example.com");
+    });
+
+    it("should  create from string", () => {
+      expect(Email.createFromString("joe_doe@example.com").getValue()).toBe("joe_doe@example.com");
+    });
+  });
+
+  describe("failure", () => {
+    it("should not create an invalid email", () => {
+      expect(Email.create("joe.doe@.com").isErr).toBe(true);
+    });
+  });
+});
---
+++ src/core/valueObjects/__tests__/Password.test.ts
@@ -0,0 +1,34 @@
+import { Password } from "@core/valueObjects/Password";
+import { HashInMemory } from "@shared/__tests__/inMemory/HashInMemory";
+
+describe("Password value object", () => {
+  describe("sucess", () => {
+    it("should create a valid password", async () => {
+      const hasher = new HashInMemory();
+      expect(await Password.create("@Password123", hasher).then((x) => x.isOk)).toBe(true);
+      expect(await Password.create("@Password123", hasher).then((x) => x.value.getValue())).toBe(
+        "hashed-@Password123",
+      );
+    });
+
+    it("should  create from string", async () => {
+      expect(Password.createFromString("hashed-@Password123").getValue()).toBe(
+        "hashed-@Password123",
+      );
+    });
+
+    it("should compare password", async () => {
+      const hasher = new HashInMemory();
+      expect(
+        await Password.compare("@Password123", "hashed-@Password123", hasher).then((x) => x),
+      ).toBe(true);
+    });
+  });
+
+  describe("failure", () => {
+    it("should not create an invalid password", async () => {
+      const hasher = new HashInMemory();
+      expect(await Password.create("@Passwor", hasher).then((x) => x.isErr)).toBe(true);
+    });
+  });
+});
