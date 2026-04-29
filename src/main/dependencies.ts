5a6b04a9ed3040bf30f7ebec566cd8aa1adf7865
filhotera <igor.pignatari@hotmail.com>
2026-04-29 18:16:02 +0100

    refactor(main): update dependecies

--- src/main/dependencies.ts
+++ src/main/dependencies.ts
@@ -1,9 +1,13 @@
 import { HashInMemory } from "@shared/__tests__/inMemory/HashInMemory";
+import { env } from "@shared/env/env";
+import { PgPromiseAdapter } from "@shared/infra/database/adapters/PgPromiseAdapter";
+import { getDbConnection } from "@shared/infra/database/connections/PgPromiseConnection";
 import { PinoLogger } from "@shared/logger/PinoLogger";
 
 export type Dependencies = typeof dependencies;
 
 export const dependencies = {
   hash: new HashInMemory(),
+  db: new PgPromiseAdapter(getDbConnection(env.databaseUrl)),
   logger: PinoLogger.create(),
 } as const;
