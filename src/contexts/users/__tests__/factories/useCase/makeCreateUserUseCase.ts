257470af7a6bb2b0e1751f981441a07fe1c7e739
filhotera <igor.pignatari@hotmail.com>
2026-04-30 01:38:06 +0100

    refactor(contexts/users/__tests__): improve factories, inMemory and mocks

--- src/contexts/users/__tests__/factories/useCase/makeCreateUserUseCase.ts
+++ src/contexts/users/__tests__/factories/useCase/makeCreateUserUseCase.ts
@@ -1,12 +1,16 @@
+import type { UserRepository } from "@contexts/users/application/ports/output/UserRepository";
 import { CreateUserUseCase } from "@contexts/users/application/useCases/CreateUserUseCase";
 import { HashInMemory } from "@shared/__tests__/inMemory/HashInMemory";
 import { UserRepositoryInMemory } from "../../inMemory/UserRepositoryInMemory";
-import type { SutType } from "../types/Sut";
 
-export const makeCreateUserUseCase = (deps?: Partial<SutType>) => {
-  const userRepostory = deps?.userRepository ?? new UserRepositoryInMemory();
+type Dependencies = {
+  userRepository: UserRepository;
+};
+
+export const makeCreateUserUseCase = (deps?: Partial<Dependencies>) => {
+  const userRepository = deps?.userRepository ?? new UserRepositoryInMemory();
   const hasher = new HashInMemory();
-  const useCase = new CreateUserUseCase(userRepostory, hasher);
+  const useCase = new CreateUserUseCase(userRepository, hasher);
 
   return {
     useCase,
