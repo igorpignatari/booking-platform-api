1e7787e5924a542507ec10f4fd3e4d739e829898
filhotera <igor.pignatari@hotmail.com>
2026-04-30 01:16:15 +0100

    refactor(contexts/users/domain): improve TPersistedUser type

--- src/contexts/users/domain/types/TPersistedUser.ts
+++ src/contexts/users/domain/types/TPersistedUser.ts
@@ -1,9 +1,10 @@
-import type { CreateUserRequest } from "@contexts/users/application/DTOs/createUserDTO";
-import type { Role } from "../valueObjects/Role";
-
-export type TPersistedUser = CreateUserRequest & {
+export type TPersistedUser = {
   id: string;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
   createdAt: Date;
   updatedAt: Date;
-  role: Role;
+  role: string;
 };
