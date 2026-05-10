0122d9c556e78483a248ea79df03b11cd83d1ca4
filhotera <igor.pignatari@hotmail.com>
2026-05-10 16:21:46 +0100

    feat(contexts/auth/domain): add refresh token already exists error and refresh token not found error

--- src/contexts/auth/domain/errors/AuthErrors.ts
+++ src/contexts/auth/domain/errors/AuthErrors.ts
@@ -1,4 +1,9 @@
-import { ForbiddenError, UnauthorizedError } from "@shared/errors/httpErrors";
+import {
+  ConflictError,
+  ForbiddenError,
+  NotFoundError,
+  UnauthorizedError,
+} from "@shared/errors/httpErrors";
 
 class UserUnauthorizedError extends UnauthorizedError {
   private constructor(message: string) {
@@ -30,8 +35,30 @@ class UserLoginError extends UnauthorizedError {
   }
 }
 
+class RefreshTokenNotFoundError extends NotFoundError {
+  private constructor(message: string) {
+    super(message, "REFRESH_TOKEN_NOT_FOUND");
+  }
+
+  static create(message: string): RefreshTokenNotFoundError {
+    return new RefreshTokenNotFoundError(message);
+  }
+}
+
+class RefreshTokenAlreadyExistsError extends ConflictError {
+  private constructor(message: string) {
+    super(message, "REFRESH_TOKEN_ALREADY_EXISTS");
+  }
+
+  static create(message: string): RefreshTokenAlreadyExistsError {
+    return new RefreshTokenAlreadyExistsError(message);
+  }
+}
+
 export const AuthErrors = {
   USER_UNAUTHORIZED_ERROR: UserUnauthorizedError,
   USER_FORBIDDEN_ERROR: UserForbiddenError,
   USER_LOGIN_ERROR: UserLoginError,
+  REFRESH_TOKEN_NOT_FOUND: RefreshTokenNotFoundError,
+  REFRESH_TOKEN_ALREADY_EXISTS: RefreshTokenAlreadyExistsError,
 } as const;
